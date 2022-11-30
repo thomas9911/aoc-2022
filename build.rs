3a416ef4fcbfdccc06e3f70e30fe2e0915052eb4
Thomas <thomastimmer11@hotmail.com>
2022-11-30 17:29:50 +0100

    fix: dynamically include include folder in build.rs

--- build.rs
+++ build.rs
@@ -1,23 +1,20 @@
 fn main() {
     println!("cargo:rustc-link-search=lib");
     println!("cargo:rerun-if-changed=lib");
+    println!("cargo:rerun-if-changed=include");
 
-    // let bindings = bindgen::Builder::default()
-    //     .header("lib/wrapper.h")
-    //     .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-    //     .rustfmt_bindings(true)
-    //     .generate()
-    //     .expect("Unable to generate bindings");
-
-    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    // bindings
-    //     .write_to_file(out_path.join("bindings.rs"))
-    //     .expect("Couldn't write bindings!");
+    let includes = std::fs::read_dir("include").unwrap().flat_map(|x| {
+        let entry = x.unwrap();
+        if entry.file_type().unwrap().is_dir() {
+            Some(entry.path())
+        } else {
+            None
+        }
+    });
 
     cc::Build::new()
         .include("lib")
-        .include("include/CLIST")
-        .include("include/hashmap.c")
+        .includes(includes)
         .file("include/CLIST/clist.c")
         .file("include/hashmap.c/hashmap.c")
         .file("lib/wrapper.c")
