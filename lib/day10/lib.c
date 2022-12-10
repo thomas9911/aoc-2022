3f6b135dcb97109ed32b43f545e6f638ebd89d00
Thomas <thomastimmer11@hotmail.com>
2022-12-10 18:31:21 +0100

    chore: combine day10 part a and b into one function

--- lib/day10/lib.c
+++ lib/day10/lib.c
@@ -6,7 +6,7 @@
 #define SCREEN_X 40
 #define SCREEN_Y 6
 
-void tick_cycle_part_a(int *state_x, int *cycles, int *signal_strength_recorder)
+void tick_cycle_part_a(int *state_x, int *cycles, int *signal_strength_recorder, unsigned char *output)
 {
     (*cycles)++;
     if (*cycles % 40 == 20)
@@ -15,7 +15,7 @@ void tick_cycle_part_a(int *state_x, int *cycles, int *signal_strength_recorder)
     }
 }
 
-void tick_cycle_part_b(int *state_x, int *cycles, unsigned char *output)
+void tick_cycle_part_b(int *state_x, int *cycles, int *signal_strength_recorder, unsigned char *output)
 {
     int current_cycle = (*cycles) % SCREEN_X;
     if (current_cycle == 0 && *cycles != 0)
@@ -34,7 +34,7 @@ void tick_cycle_part_b(int *state_x, int *cycles, unsigned char *output)
     (*cycles)++;
 }
 
-long day10a(const char *text)
+long run_day10(const char *text, unsigned char *output, void tick_function(int *state_x, int *cycles, int *signal_strength_recorder, unsigned char *output))
 {
     const char seperator[2] = "\n";
     char *line;
@@ -52,12 +52,12 @@ long day10a(const char *text)
     {
         if (strcmp(line, NOOP) == 0)
         {
-            tick_cycle_part_a(&state_x, &cycles, &signal_strength_sum);
+            tick_function(&state_x, &cycles, &signal_strength_sum, output);
         }
         else if (strncmp(line, ADDX, strlen(ADDX)) == 0)
         {
-            tick_cycle_part_a(&state_x, &cycles, &signal_strength_sum);
-            tick_cycle_part_a(&state_x, &cycles, &signal_strength_sum);
+            tick_function(&state_x, &cycles, &signal_strength_sum, output);
+            tick_function(&state_x, &cycles, &signal_strength_sum, output);
             arg1 = atoi(line + strlen(ADDX));
             state_x += arg1;
         };
@@ -68,34 +68,12 @@ long day10a(const char *text)
     return signal_strength_sum;
 }
 
+long day10a(const char *text)
+{
+    return run_day10(text, NULL, tick_cycle_part_a);
+}
+
 long day10b(const char *text, unsigned char *output)
 {
-    const char seperator[2] = "\n";
-    char *line;
-
-    int arg1 = 0;
-
-    int state_x = 1;
-    int cycles = 0;
-
-    line = strtok(text, seperator);
-
-    while (line != NULL)
-    {
-        if (strcmp(line, NOOP) == 0)
-        {
-            tick_cycle_part_b(&state_x, &cycles, output);
-        }
-        else if (strncmp(line, ADDX, strlen(ADDX)) == 0)
-        {
-            tick_cycle_part_b(&state_x, &cycles, output);
-            tick_cycle_part_b(&state_x, &cycles, output);
-            arg1 = atoi(line + strlen(ADDX));
-            state_x += arg1;
-        };
-
-        line = strtok(NULL, seperator);
-    }
-
-    return 1;
+    return run_day10(text, output, tick_cycle_part_b);
 }
